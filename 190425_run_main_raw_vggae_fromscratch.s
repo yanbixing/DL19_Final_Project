406f934cbb29952c525150ad51dc8e8753819509
Bixing Yan <by783@nyu.edu>
2019-04-25 23:39:00 -0400

    190425

--- 190425_run_main_raw_vggae_fromscratch.s
+++ 190425_run_main_raw_vggae_fromscratch.s
@@ -9,5 +9,5 @@
 module purge
 source ~/myenv/bin/activate
 
-python -c "print('begin_raw_vggae_en_pretrained_model')"
-python 190425_main_raw_vggae.py --model vgg --batch-size 1024 --save 190425_raw_vggae_en_pretrained.pt --epochs 50 --pretrained False
+python -c "print('begin_raw_vggae_fromscratch_model')"
+python 190425_main_raw_vggae.py --model vgg --batch-size 1024 --save 190425_raw_vggae_fromscratch.pt --epochs 50 --pretrained False
