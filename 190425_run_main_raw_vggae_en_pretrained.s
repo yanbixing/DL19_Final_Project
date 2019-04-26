e5b3891c3da2dd5c93c1d97add104ed8514b9e31
Bixing Yan <by783@nyu.edu>
2019-04-26 08:01:39 -0400

    190426

--- 190425_run_main_raw_vggae_en_pretrained.s
+++ 190425_run_main_raw_vggae_en_pretrained.s
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:v100:4
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=16GB
+#SBATCH --mem=36GB
 #SBATCH --time=72:00:00
 
 module purge
 source ~/myenv/bin/activate
 
 python -c "print('begin_raw_vggae_en_pretrained_model')"
-python 190425_main_raw_vggae.py --model vgg --batch-size 1024 --save 190425_raw_vggae_en_pretrained.pt --epochs 50 --pretrained True
+python 190425_main_raw_vggae.py --model vgg --batch-size 768 --save 190425_raw_vggae_en_pretrained.pt --epochs 50 --pretrained True
