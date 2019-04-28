9b9fb4fe09fda3cf6b28a7ad796990148ad1c8e7
Bixing Yan <by783@nyu.edu>
2019-04-28 09:49:53 -0400

    190428

---
+++ 190427_run_main_dn_vggae_fromscratch_n30pc_small.s
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:v100:1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=16GB
+#SBATCH --time=48:00:00
+
+module purge
+source ~/myenv/bin/activate
+
+python -c "print('begin_dn_vggae_fromscratch_model')"
+python 190427_main_dn_vggae.py --model vgg --batch-size 1024 --save 190425_dn_vggae_fromscratch_30pc.pt --epochs 40 --pretrained False --noise-level 0.066
