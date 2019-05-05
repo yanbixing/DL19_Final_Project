de7af98ac308f9e0324915e106b2de06d0632477
Bixing Yan <by783@nyu.edu>
2019-05-05 16:20:37 -0400

    190505_4pm20

---
+++ 190505_run_main_SDvggAE_retrain_lr001.s
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=SDvggAE_001
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:v100:2
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=24GB
+#SBATCH --time=48:00:00
+
+module purge
+source ~/myenv/bin/activate
+
+python -c "print('190505_main_SDvggAE_retrain.py')"
+python 190505_main_SDvggAE_retrain.py --model vgg --model-folder /scratch/by783/DL_Final_models/ --model-file 190504_SDvggAE_D01_lr001.pt --batch-size 512 --save 190505SDvggAE_D01_lr001.pt --epochs 300 --lr 0.01
