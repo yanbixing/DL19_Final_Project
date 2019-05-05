6c7860a3062f4bda99a919b2c17df1de2803b452
Bixing Yan <by783@nyu.edu>
2019-05-05 14:27:14 -0400

    190505

--- 190505_run_main_SDvggAE_retrain.s
+++ 190505_run_main_SDvggAE_retrain.s
@@ -11,4 +11,4 @@ module purge
 source ~/myenv/bin/activate
 
 python -c "print('190505_main_SDvggAE_retrain.py')"
-python 190505_main_SDvggAE_retrain.py --model vgg --model_folder /scratch/by783/DL_Final_models/ --model-file 190504_SDvggAE_D01.pt --batch-size 512 --save 190505SDvggAE_D01.pt_try --epochs 50 --lr 0.001
+python 190505_main_SDvggAE_retrain.py --model vgg --model_folder /scratch/by783/DL_Final_models/ --model-file 190504_SDvggAE_D01.pt --batch-size 512 --save 190505SDvggAE_D01.pt_try --epochs 300 --lr 0.001
