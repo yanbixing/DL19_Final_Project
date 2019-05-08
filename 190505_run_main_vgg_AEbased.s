41d1bc2e2962064e9d4ca54345c94d13d164ea9f
Bixing Yan <by783@nyu.edu>
2019-05-08 11:45:36 -0400

    190508_11am37

--- 190505_run_main_vgg_AEbased.s
+++ 190505_run_main_vgg_AEbased.s
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=vgg_AEB
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:p100:1
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=16GB
 #SBATCH --time=48:00:00
@@ -11,4 +11,4 @@ module purge
 source ~/myenv/bin/activate
 
 python -c "print('vgg_AE_based')"
-python 190505_main_vgg_AEbased.py --model vgg --AE-file 190505SDvggAE_D01_lr001.pt --batch-size 256 --feature-pinning True --save 190507_vgg_AEB_lr001.pt --epochs 500 --lr 0.01
+python 190505_main_vgg_AEbased.py --model vgg --AE-file 190507SDvggAE_D01_lr005.pt --batch-size 256 --feature-pinning True --save 190508_vgg_AEB_lr005.pt --epochs 500 --lr 0.01
