3e2a213a93e9ff1c18534b2b7b4964d0116d94f5
Bixing Yan <by783@nyu.edu>
2019-05-10 11:17:52 -0400

    190510

--- 190510_main_vgg_fcadj_daug_retrain.py
+++ 190510_main_vgg_fcadj_daug_retrain.py
@@ -65,20 +65,26 @@ else:
     sys.stdout.write('Warning, CPU mode, pls check')
 
 def image_loader(path, batch_size):
-    transform = transforms.Compose(
-        [
-            #transforms.Resize(input_size),
-            #transforms.CenterCrop(input_size),
-            # use model fitted with the image size, so no need to resize
+    transform = {
+        'train':transforms.Compose([
+            transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(10),
+            transforms.RandomAffine(10),
+            transforms.RandomResizedCrop(size=96,scale=(0.9, 1.0)),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-            # https://pytorch.org/docs/stable/torchvision/transforms.html
-            # [mean],[std] for different channels
-        ]
-    )
-    sup_train_data = datasets.ImageFolder('{}/{}/train'.format(path, 'supervised'), transform=transform)
-    sup_val_data = datasets.ImageFolder('{}/{}/val'.format(path, 'supervised'), transform=transform)
-    unsup_data = datasets.ImageFolder('{}/{}/'.format(path, 'unsupervised'), transform=transform)
+        ]),
+        'val':transforms.Compose([
+            #transforms.Resize(96),
+            #transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    }
+    sup_train_data = datasets.ImageFolder('{}/{}/train'.format(path, 'supervised'), transform=transform['train'])
+    sup_val_data = datasets.ImageFolder('{}/{}/val'.format(path, 'supervised'), transform=transform['val'])
+    unsup_data = datasets.ImageFolder('{}/{}/'.format(path, 'unsupervised'), transform=transform['val'])
     # source code: https://github.com/pytorch/vision/blob/master/torchvision/datasets/folder.py
     # Main idea:
     data_loader_sup_train = torch.utils.data.DataLoader(
